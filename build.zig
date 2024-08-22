372ab8992bd7f411a763b6bfa8d68941d6866cda
fjebaker <fergusbkr@gmail.com>
2024-08-22 18:17:15 +0100

    build.zig: expose the datatype option

--- build.zig
+++ build.zig
@@ -1,7 +1,21 @@
 const std = @import("std");
 
+pub const DataType = enum {
+    float,
+    double,
+    int16,
+    int32,
+    simd,
+};
+
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
+
+    const dtype = b.option(
+        DataType,
+        "KISSFFT_DATATYPE",
+        "The datatype to use (default: float).",
+    ) orelse .float;
 
     const lib = b.addStaticLibrary(.{
         .name = "kissfft",
@@ -10,6 +24,7 @@ pub fn build(b: *std.Build) !void {
     });
     lib.linkLibC();
     lib.addCSourceFile(.{ .file = b.path("kiss_fft.c") });
+    lib.defineCMacro("KISSFFT_DATATYPE", @tagName(dtype));
     lib.installHeader(b.path("kiss_fft.h"), "kiss_fft.h");
 
     b.installArtifact(lib);
